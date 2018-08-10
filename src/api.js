1e43685b051725c917da46440dd3c0f8361b6d94
Sergey Malinin <sergmalinin@gmail.com>
2018-08-10 21:47:04 +1000

    Update for use HTTP POST instead of GET for /logout

--- src/api.js
+++ src/api.js
@@ -88,21 +88,13 @@ export async function logout(
   storage: AsyncStorage = defaultStorage()
 ): Promise<void> {
   const session = await getSession(storage)
-  if (!session) {
-    return
+  if (session) {
+    try {
+      await WebIdOidc.logout(storage, session.idp)
+    } catch (err) {
+      console.warn('Error logging out:')
+      console.error(err)
+    }
+    await clearSession(storage)
   }
-  switch (session.authType) {
-    case 'WebID-OIDC':
-      try {
-        await WebIdOidc.logout(storage, session.idp)
-      } catch (err) {
-        console.warn('Error logging out:')
-        console.error(err)
-      }
-      break
-    case 'WebID-TLS':
-    default:
-      break
-  }
-  return clearSession(storage)
 }
